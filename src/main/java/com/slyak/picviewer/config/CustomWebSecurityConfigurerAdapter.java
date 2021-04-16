5b0395591712546ad8ae159ea3587f44159117cf
周宁 <stormning@163.com>
2021-04-16 16:46:24 +0800

    basic auth

---
+++ src/main/java/com/slyak/picviewer/config/CustomWebSecurityConfigurerAdapter.java
@@ -0,0 +1,30 @@
+package com.slyak.picviewer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().sameOrigin().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.
+                inMemoryAuthentication()
+                .withUser("ybb")
+                .password("ybb")
+                .roles("USER", "ADMIN");
+    }
+}
